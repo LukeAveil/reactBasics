891ddd7d46f94b7a7a06883145119564a80d16f6
Luke <lukeaveil@yahoo.com>
2016-09-30 18:07:42 +0100

    adds two basic components

---
+++ src/app/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Header extends React.Component {
+  render() {
+    return(
+      <div>
+        <ul>
+          <li><a href="#">Home</a></li>
+        </ul>
+      </div>
+    );
+  }
+}
